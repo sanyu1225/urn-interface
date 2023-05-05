fd8e45e81eca92690310b260171aa9bd876c1519
andrew54068 <andrew0424718012@yahoo.com.tw>
2023-05-05 18:32:26 +0800

    feat: support all wallets.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,8 +6,18 @@ import {
   NetworkName,
 } from '@aptos-labs/wallet-adapter-react';
 import { Client as UrqlClient, Provider as UrqlProvider, cacheExchange, fetchExchange } from 'urql';
-import { BloctoWallet } from '@blocto/aptos-wallet-adapter-plugin';
-import { PetraWallet } from 'petra-plugin-wallet-adapter';
+import { BloctoWallet } from "@blocto/aptos-wallet-adapter-plugin";
+import { FewchaWallet } from "fewcha-plugin-wallet-adapter";
+import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
+import { NightlyWallet } from "@nightlylabs/aptos-wallet-adapter-plugin";
+import { OpenBlockWallet } from "@openblockhq/aptos-wallet-adapter";
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+import { PontemWallet } from "@pontem/wallet-adapter-plugin";
+import { RiseWallet } from "@rise-wallet/wallet-adapter";
+import { TokenPocketWallet } from "@tp-lab/aptos-wallet-adapter";
+import { TrustWallet } from "@trustwallet/aptos-wallet-adapter";
+import { MSafeWalletAdapter } from "msafe-plugin-wallet-adapter";
+import { WelldoneWallet } from "@welldone-studio/aptos-wallet-adapter";
 import { theme } from '../theme';
 import { ContextProvider } from '../context';
 import { supportWebp } from '../utils';
@@ -18,17 +28,17 @@ const wallets = [
     network: NetworkName.Testnet,
     bloctoAppId: '6d85f56e-5f2e-46cd-b5f2-5cf9695b4d46',
   }),
-  //   new FewchaWallet(),
-  //   new MartianWallet(),
-  //   new MSafeWalletAdapter(),
-  //   new NightlyWallet(),
-  //   new OpenBlockWallet(),
+  new FewchaWallet(),
+  new MartianWallet(),
+  new MSafeWalletAdapter(),
+  new NightlyWallet(),
+  new OpenBlockWallet(),
   new PetraWallet(),
-  //   new PontemWallet(),
-  //   new RiseWallet(),
-  //   new TokenPocketWallet(),
-  //   new TrustWallet(),
-  //   new WelldoneWallet(),
+  new PontemWallet(),
+  new RiseWallet(),
+  new TokenPocketWallet(),
+  new TrustWallet(),
+  new WelldoneWallet(),
 ];
 const client = new UrqlClient({
   // TODO: set env file for graphql endpoint
