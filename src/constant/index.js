7843cfea1a14d53f8cde3cc5e4e2eccfedef4dd4
andrew54068 <andrew0424718012@yahoo.com.tw>
2023-06-18 15:24:51 +0800

    feat: add shovel query.

--- src/constant/index.js
+++ src/constant/index.js
@@ -7,20 +7,63 @@ export const CREATOR_ADDRESS = process.env.REACT_CREATOR_ADDRESS || '0xa9186f2d8
 export default CONTRACT_ADDR;
 
 export const queryAltarData = gql`
-  query CurrentTokens($address: String,$creator_address: String) {
+  query CurrentTokens($address: String, $creator_address: String) {
     current_token_ownerships(
-        where: {owner_address: {_eq: $address}, amount: {_gt: "0"}, table_type: {_eq: "0x3::token::TokenStore"}, current_token_data: {name: {}}, collection_name: {_eq: "urn"},creator_address: {_eq: $creator_address}}
+      where: {
+        owner_address: { _eq: $address }
+        amount: { _gt: "0" }
+        table_type: { _eq: "0x3::token::TokenStore" }
+        current_token_data: { name: {} }
+        collection_name: { _eq: "urn" }
+        creator_address: { _eq: $creator_address }
+      }
     ) {
-        token_data_id_hash
-        name
-        collection_name
-        property_version
-        amount
-        token_properties
-        # default_properties
-        current_token_data {
-          ...TokenDataFields
-        }
+      token_data_id_hash
+      name
+      collection_name
+      property_version
+      amount
+      token_properties
+      # default_properties
+      current_token_data {
+        ...TokenDataFields
+      }
+    }
+  }
+  fragment TokenDataFields on current_token_datas {
+    creator_address
+    collection_name
+    description
+    metadata_uri
+    name
+    token_data_id_hash
+    collection_data_id_hash
+    default_properties
+  }
+`;
+
+export const queryShovelData = gql`
+  query CurrentTokens($address: String, $creator_address: String) {
+    current_token_ownerships(
+      where: {
+        owner_address: { _eq: $address }
+        amount: { _gt: "0" }
+        table_type: { _eq: "0x3::token::TokenStore" }
+        current_token_data: { name: { _eq: "shovel" } }
+        collection_name: { _eq: "urn" }
+        creator_address: { _eq: $creator_address }
+      }
+    ) {
+      token_data_id_hash
+      name
+      collection_name
+      property_version
+      amount
+      token_properties
+      # default_properties
+      current_token_data {
+        ...TokenDataFields
+      }
     }
   }
   fragment TokenDataFields on current_token_datas {
@@ -36,21 +79,28 @@ export const queryAltarData = gql`
 `;
 
 export const queryUrnData = gql`
-  query CurrentTokens($address: String, $offset: Int,$creator_address: String) {
+  query CurrentTokens($address: String, $offset: Int, $creator_address: String) {
     current_token_ownerships(
-        where: {owner_address: {_eq: $address}, amount: {_gt: "0"}, table_type: {_eq: "0x3::token::TokenStore"}, current_token_data: {name: {_eq: "urn"}}, collection_name: {_eq: "urn"},creator_address: {_eq: $creator_address}}
-        offset: $offset
+      where: {
+        owner_address: { _eq: $address }
+        amount: { _gt: "0" }
+        table_type: { _eq: "0x3::token::TokenStore" }
+        current_token_data: { name: { _eq: "urn" } }
+        collection_name: { _eq: "urn" }
+        creator_address: { _eq: $creator_address }
+      }
+      offset: $offset
     ) {
-        token_data_id_hash
-        name
-        collection_name
-        property_version
-        amount
-        token_properties
-        # default_properties
-        current_token_data {
-          ...TokenDataFields
-        }
+      token_data_id_hash
+      name
+      collection_name
+      property_version
+      amount
+      token_properties
+      # default_properties
+      current_token_data {
+        ...TokenDataFields
+      }
     }
   }
   fragment TokenDataFields on current_token_datas {
