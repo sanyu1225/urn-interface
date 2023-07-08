ba076d328324c74e5ce724e99759a69465a1acf5
sanyu1225 <pan831225@gmail.com>
2023-07-09 02:59:23 +0800

    feat: update robdata url

--- src/hooks/useRobData.js
+++ src/hooks/useRobData.js
@@ -10,8 +10,7 @@ const useRobData = (account) => {
         if (!account) return;
         setLoading(true);
         // TODO: set env file for endpoint
-        const fetchUrl = 'https://fullnode.testnet.aptoslabs.com/v1/accounts/0xb34c0314d90b2597f2531119601f4ad7fe9db4eb7671265e93c905a46aa92860/events/0x56260c419e8b176e0ca7f6d439b69180c2de2cb284d8dee24476f247af204492::knife::RobHistory/been_robbed_events';
-        // const fetchUrl = `https://fullnode.testnet.aptoslabs.com/v1/accounts/${account}/events/${CONTRACT_ADDR}::knife::RobHistory/been_robbed_events`;
+        const fetchUrl = `https://fullnode.testnet.aptoslabs.com/v1/accounts/${account}/events/${CONTRACT_ADDR}::knife::RobHistory/been_robbed_events`;
         fetch(fetchUrl, {
             method: 'GET',
             headers: {
