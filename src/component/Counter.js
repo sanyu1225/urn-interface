32f06b5940c3ead67780111b0c878feccbe73729
sanyu1225 <pan831225@gmail.com>
2024-01-19 01:57:10 +0800

    fix: counter css

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 
 import minus from '../assets/images/icons/minus.svg';
 import plus from '../assets/images/icons/plus.svg';
@@ -11,28 +11,39 @@ const Counter = ({ defaultValue, onChange }) => {
   const [count, setCount] = useState(defaultValue);
 
   return (
-    <Flex bg="#FFE5B4" justifyContent="space-between" alignItems="center" p="6px 10px" w="100%" h="32px" borderRadius="100px">
-      <Image
-        cursor="pointer"
-        alt="reduce"
-        src={minus}
-        onClick={() => {
-          const newValue = Math.max(1, count - 1);
-          setCount(newValue);
-          onChange(newValue);
-        }}
-      />
+    <Flex
+      bg="#FFE5B4"
+      justifyContent="space-between"
+      alignItems="center"
+      p="6px 10px"
+      w="100%"
+      h="32px"
+      borderRadius="100px"
+    >
+      <Box cursor="pointer">
+        <Image
+          alt="reduce"
+          src={minus}
+          onClick={() => {
+            const newValue = Math.max(1, count - 1);
+            setCount(newValue);
+            onChange(newValue);
+          }}
+        />
+      </Box>
       {count}
-      <Image
-        cursor="pointer"
-        alt="increase"
-        src={plus}
-        onClick={() => {
-          const newValue = count + 1;
-          setCount(newValue);
-          onChange(newValue);
-        }}
-      />
+      <Box cursor="pointer">
+        <Image
+          cursor="pointer"
+          alt="increase"
+          src={plus}
+          onClick={() => {
+            const newValue = count + 1;
+            setCount(newValue);
+            onChange(newValue);
+          }}
+        />
+      </Box>
     </Flex>
   );
 };
